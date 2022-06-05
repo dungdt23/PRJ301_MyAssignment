7d01b15759c69552923aad42bdb888c15768a879
dungdt23 <dungtiendao01@gmail.com>
2022-06-05 23:26:36 +0700

    create LoginInterface
    create basic login interface and connect with database

---
+++ MyAssignment/src/java/dal/DBContext.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ *
+ */
+public abstract class DBContext<T> {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String user = "dungdt";
+            String pass = "12345";
+            String url = "jdbc:sqlserver://fred\\SQLEXPRESS:1433;databaseName=PRJ_MyAssignment";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public abstract ArrayList<T> list();
+
+    public abstract T get(T entity);
+
+    public abstract void insert(T entity);
+
+    public abstract void update(T entity);
+
+    public abstract void delete(T entity);
+
+}
---
+++ MyAssignment/web/view/authentication/login.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : authentication
+    Created on : Jun 5, 2022, 10:41:18 PM
+    Author     : Admin
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="view/authentication/authen.css" rel="stylesheet" type="text/css" />
+        <title>My Assignment</title>
+    </head>
+    <body>
+        <form action="authentication" method="POST">
+            Campus: <select name="campus">
+                <option>FU-HL</option>
+            </select> <Br/>
+            Lecture: <input type="text" name="lecture"/>
+            <input type="submit" value="View"/>
+        </form>
+    </body>
+</html>
